cb141659d3d70c09d627905dc77041a88cafabd2
ZeeAppDeveloper <99582811+ZeeAppDeveloper@users.noreply.github.com>
2023-09-02 22:34:27 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -134,7 +134,7 @@ def start_matching(message):
         
 
         if match_result:
-            match_id, match_username, match_age, match_region, match_info, match_foto1 = match_result
+          match_id, match_username, match_age, match_region, match_info, match_foto1 = match_result
 
           markup = types.ReplyKeyboardMarkup(row_width=2, resize_keyboard=True)
           markup.add(types.KeyboardButton("❤️"), types.KeyboardButton("👎")) 
