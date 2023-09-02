babed6ab3f7ca5fa3245c00448887c3c3dc87247
ZeeAppDeveloper <99582811+ZeeAppDeveloper@users.noreply.github.com>
2023-09-03 00:25:55 +0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ logging.basicConfig(level=logging.INFO)
 bot = telebot.TeleBot(API_TOKEN)
 
 # MySQL database connection information
-DB_HOST = 'sql310.byetcluster.com'
-DB_NAME = 'userdbtelegram'
+DB_HOST = 'sql310.liveblog365.com'
+DB_NAME = 'lblog_34952417_userdbtelegram'
 DB_USER = 'lblog_34952417'
 DB_PASSWORD = 'Xalid1234'
 
